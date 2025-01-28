f31cd97f6a2400987ef00a2418572f204ee2d27e
dam <m.maeda@newbees.jp>
2025-01-28 15:14:23 +0900

    ./utils/buildUtils.jsをインポートする形にし、StyleDictionary.registerFormatのformatを変更。

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 import StyleDictionary from 'style-dictionary';
+import { processColorTokens } from './utils/buildUtils.js';
 
 // Semantic生成用フィルタ
 StyleDictionary.registerFilter({
@@ -11,63 +12,22 @@ StyleDictionary.registerFormat({
   name: 'colorset',
   format: function ({ dictionary }) {
     // Contents.json の基本構造
-    const colorSet = {
+    const baseColorSet = {
       colors: [],
       info: {
-        author: "xcode",
+        author: 'xcode',
         version: 1,
       },
       properties: {
         localizable: true,
       },
     };
-    
     // dictionary.allTokens ですべてのトークンを配列化
-    dictionary.allTokens.forEach((token) => {
-      const colorValue = token.value; // HEX値を取得
-      const rgba = hexToRgba(colorValue); // HEX値をRGBaに変換
-      
-      // トークンの形式を作成
-      const colorData = {
-        color: {
-          "color-space": "srgb",
-          components: {
-            alpha: rgba.alpha.toFixed(3),
-            red: rgba.red.toFixed(3),
-            green: rgba.green.toFixed(3),
-            blue: rgba.blue.toFixed(3)
-          },
-        },
-        idiom: "universal",
-      };
-      
-      colorSet.colors.push(colorData);
+    const colorTokens = processColorTokens(dictionary.allTokens);
 
-    });
-    
-    return JSON.stringify(colorSet, null, 2);
-
+    return JSON.stringify({ ...baseColorSet, colors: colorTokens }, null, 2);
   },
 });
-
-// HEX値をRGBaに変換する関数
-function hexToRgba(hex) {
-  hex = hex.replace(/^#/, '');
-  
-  if ( hex.length === 6 || hex.length === 8 ) {
-    const r = parseInt(hex.slice(0, 2), 16) / 255;
-    const g = parseInt(hex.slice(2, 4), 16) / 255;
-    const b = parseInt(hex.slice(4, 6), 16) / 255;
-    const a = hex.length === 8 ? parseInt(hex.slice(6, 8), 16) / 255 : 1;
-    
-    return {
-      red: r,
-      green: g,
-      blue: b,
-      alpha: a
-    };
-  };
-};
 
 const sd = new StyleDictionary('config.js');
 
